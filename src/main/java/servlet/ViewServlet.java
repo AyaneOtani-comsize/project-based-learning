f2da570c73ed7db3efcacdff39a7c78a691e6430
AyaneOtani-comsize <ayane.otani@comsize.com>
2026-07-30 13:21:13 +0900

    ViewServlet完成。view.jspは途中まで作成。

--- src/main/java/servlet/ViewServlet.java
+++ src/main/java/servlet/ViewServlet.java
@@ -1,12 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.TaskDAO;
+import model.entity.TaskBean;
 
 /**
  * Servlet implementation class ViewServlet
@@ -27,16 +33,33 @@ public class ViewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//ログイン画面へ転送
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//t_taskの情報を取得してview.jspに表示させる
+		//TaskDAOのインスタンスを生成
+		TaskDAO taskDAO = new TaskDAO();
 		
+		//タスクリスト格納用変数を宣言
+		List<TaskBean> taskList = new ArrayList<>();
+		
+		//DBからタスクリストを取得
+		try {
+			taskList = taskDAO.selectAll();
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("処理結果：異常が発生しました。");
+			e.printStackTrace();
+		}
+		
+		//タスクリストをリクエストスコープに格納
+		request.setAttribute("taskList", taskList);
+		
+		//タスク一覧表示画面へ遷移
+		request.getRequestDispatcher("view.jsp").forward(request, response);
 	}
 
 }
--- src/main/webapp/view.jsp
+++ src/main/webapp/view.jsp
@@ -4,13 +4,24 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>タスク一覧表示画面</title>
 </head>
 <body>
 	<h1>タスク一覧表示</h1>
 	
 	<hr>
 	<br>
+	
+	<%
+		//
+		
+		
+		//リクエストスコープからタスクリストを取得
+		List<TaskBean> taskList = (List<TaskBean>) request.getAttribute("taskList");
+		
+		
+		
+	%>
 	
 	<table border="1" style="border-collapse: collapse; border-color: black">
 		<tr>
@@ -24,7 +35,10 @@
 		</tr>
 		
 		
+		
+		
 	</table>
+	<br>
 	<form action="menu.jsp"><input type="submit" value="メニュー画面へ"></form>
 	<form action="edit.jsp"><input type="submit" value="編集"></form>
 	<form action="rest.jsp"><input type="submit" value="削除"></form>
