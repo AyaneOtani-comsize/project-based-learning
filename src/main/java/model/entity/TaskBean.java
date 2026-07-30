77602bfeb6ac7ec48941b3029f9490ab5247fa0a
DESKTOP-PEN1SPL\USER125 <manami.miyako@comsize.com>
2026-07-30 11:38:40 +0900

    必要なファイルを全て作成。

---
+++ src/main/java/model/entity/TaskBean.java
@@ -0,0 +1,79 @@
+package model.entity;
+
+import java.util.Date;
+
+public class TaskBean {
+	/**
+	 * タスク
+	 */
+	private String taskName;
+	/**
+	 * カテゴリ
+	 */
+	private String categoryName;
+	/**
+	 * 期限
+	 */
+	private Date limitDate;
+	/**
+	 * 担当者
+	 */
+	private String userName;
+	/**
+	 * ステータス
+	 */
+	private String statusName;
+	/**
+	 * メモ
+	 */
+	private String memo;
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public Date getLimitDate() {
+		return limitDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public void setLimitDate(Date limitDate) {
+		this.limitDate = limitDate;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+}
